65de694cce5ab14cf3693d0b06930b2c26b9b374
ggpaap <gaparecidapichau@gmail.com>
2023-03-24 10:08:43 -0300

    criei classe categoria

--- livraria/models.py
+++ livraria/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
 
-# Create your models here.
+def __str__(self):
+        return self.descricao
